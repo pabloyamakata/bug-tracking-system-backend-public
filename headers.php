75d90d40c75df4f24888326b3eca57fca015ce15
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-05-09 18:06:18 -0300

    This script now allows requests from two origins.

--- headers.php
+++ headers.php
@@ -1,5 +1,10 @@
 <?php
-header('Access-Control-Allow-Origin: https://example.com'); 
+$http_origin = $_SERVER['HTTP_ORIGIN'];
+if($http_origin == "http://example1.com") {
+    header("Access-Control-Allow-Origin: http://example1.com");
+} elseif($http_origin == "http://example2.com") {
+    header("Access-Control-Allow-Origin: http://example2.com");
+}
 header("Access-Control-Allow-Credentials: true");
 header('Set-Cookie: PHPSESSID= ' . session_id() . '; SameSite=None; Secure');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
